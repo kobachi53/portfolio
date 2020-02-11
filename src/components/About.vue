c6b34dacf06a56565f965e59a3cf9899b1baf1b3
kobachi <for.sistemstudy@gmail.com>
2020-02-11 20:11:05 +0900

    【追加】 ・アバウトページの追加

---
+++ src/components/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <navber></navber>
+    <div>About</div>
+  </div>
+</template>
+
+<script>
+import Navber from "../components/Navber";
+export default {
+  name: "About",
+  components: {
+    Navber
+  },
+  data: () => ({
+    text: "Welcome　To　MyPortfolioSite"
+  })
+};
+</script>
+
+
+<style>
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <About />
 </template>
+
+<script>
+import About from "../components/About";
+export default {
+  components: {
+    About
+  }
+};
+</script>
