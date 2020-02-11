0f60244851cbd96d623f05e5e74d7ada18e17dbd
kobachi <for.sistemstudy@gmail.com>
2020-02-11 18:17:22 +0900

    【コミット漏れ】 ・新規ファイルのコミット漏れ

---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <navber></navber>
+    <div class="parent .font-weight-black.font-italic">Welcome To My Portfolio Site</div>
+  </div>
+</template>
+
+<script>
+import Navber from "../components/Navber";
+export default {
+  name: "Home",
+  components: {
+    Navber
+  },
+  data: () => ({
+    //
+  })
+};
+</script>
+
+<style>
+.parent {
+  padding: 310px 300px;
+  font-size: 70px;
+  color: #424242;
+}
+</style>
---
+++ src/components/Navber.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app-bar app color="grey darken-3">
+    <v-layout justify-space-around>
+      <div class="box">
+        <router-link to="/">
+          <font color="white">Home</font>
+        </router-link>
+      </div>
+      <div class="box">
+        <router-link to="/about">
+          <font color="white">About</font>
+        </router-link>
+      </div>
+      <div class="box">
+        <router-link to="/experiences">
+          <font color="white">Experiences</font>
+        </router-link>
+      </div>
+      <div class="box">
+        <router-link to="/works">
+          <font color="white">Works</font>
+        </router-link>
+      </div>
+      <div class="box">
+        <router-link to="/contact">
+          <font color="white">Contact</font>
+        </router-link>
+      </div>
+    </v-layout>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  data: () => ({})
+};
+</script>
+
+<style>
+.box {
+  height: 50px;
+  width: 350px;
+  background-color: #424242;
+  position: relative;
+  text-align: center;
+}
+a {
+  display: block;
+  position: absolute;
+  top: 1px;
+  left: 100;
+  width: 100%;
+  height: 100%;
+  font-size: 18px;
+  padding: 10px;
+}
+a:hover {
+  background-color: #9f956c;
+}
+</style>
